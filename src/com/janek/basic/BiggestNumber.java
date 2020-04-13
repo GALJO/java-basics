95f6216001a2ae487e7e578d3b9ada2e713fd9e3
John <jasiek.ole@gmail.com>
2020-04-13 17:44:20 +0200

    Biggest Number

---
+++ src/com/janek/basic/BiggestNumber.java
@@ -0,0 +1,41 @@
+package com.janek.basic;
+
+import static com.janek.lib.math.MathUtil.doCalculation;
+import static com.janek.lib.math.MathUtil.isDouble;
+import static java.lang.Double.parseDouble;
+import static java.lang.String.format;
+import static java.lang.System.exit;
+import static java.lang.System.out;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        validate(args);
+
+        double maxNumber = getMaxNumber(args);
+        out.println(format("Największa liczba z podanych to: %f", maxNumber));
+    }
+
+    private static double getMaxNumber(String[] args) {
+        double maxNumber = parseDouble(args[0]);
+        for (int i = 1; i < args.length; i++) {
+            if (maxNumber < parseDouble(args[i])) {
+                maxNumber = parseDouble(args[i]);
+            }
+        }
+        return maxNumber;
+    }
+
+    private static void validate(String[] args) {
+        for (String element : args) {
+            if (!isDouble(element)) {
+                out.println(format("Niepoprawny element! (%s)", element));
+                exit(1);
+            }
+        }
+
+        if (args.length == 0) {
+            out.println("Nie podałeś argumentów!");
+            exit(2);
+        }
+    }
+}
